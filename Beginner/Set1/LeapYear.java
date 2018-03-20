1baeecfae883dd88c7d0e42034f4a5aa22a9ac3e
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 16:48:29 +0530

    Create LeapYear.java

---
+++ Beginner/Set1/LeapYear.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vowelRconsonant;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Kumaran DR
+ */
+public class vowelRconsonant {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       if(sc.hasNextInt()){
+            int year = sc.nextInt();
+            if(((year%4==0)||(year%400==0))&&(year%100!=0))
+               System.out.println("yes");
+            else
+                System.out.println("no");
+        }
+        else
+           System.out.println("Invalid input");
+           
+    }
+    
+}
