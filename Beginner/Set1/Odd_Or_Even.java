ed2d7bc22d45e9a3689674657b7573b96d7a7f89
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 12:34:21 +0530

    Update Odd_Or_Even.java

--- Beginner/Set1/Odd_Or_Even.java
+++ Beginner/Set1/Odd_Or_Even.java
@@ -6,18 +6,13 @@ public class OddReven {
         Scanner sc = new Scanner(System.in);
         String result="";
         double j=0;
-        try{
-            j = sc.nextDouble();
-            if(j>100000){
-            throw new InputMismatchException ("out of the given range");
-            }
-        }
-        catch(InputMismatchException e){
-                System.out.println("Input mismatch enter oly integers and less than 1lakh and one 100001");
-            System.exit(0);
-        }        
-        result = j%2==0? "Even" : "Odd";
-        System.out.println(result);
+       j = sc.nextDouble();
+        if((j>0)&&(j>100000))
+            System.out.println("Input mismatch enter oly integers and less than 1lakh and one 100001");
+        else{
+            result = j%2==0? "Even" : "Odd";
+            System.out.println(result);
+        }       
         
     }
     
