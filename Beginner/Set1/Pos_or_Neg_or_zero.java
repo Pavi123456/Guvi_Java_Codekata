66d8653e6d7de4425143c4d85cbe8fc18ba8233f
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 09:59:25 +0530

    Update Pos_or_Neg_or_zero.java

--- Beginner/Set1/Pos_or_Neg_or_zero.java
+++ Beginner/Set1/Pos_or_Neg_or_zero.java
@@ -1,6 +1,5 @@
 
-
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -8,14 +7,19 @@ import java.util.Scanner;
  * @author Sri Kumaran DR
  */
 public class positiveOrNegOrZero {
-    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         String result="";
-        int n = sc.nextInt();
+        int n=0;
+        try{
+            n = sc.nextInt();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Input Mismatch enter only integers");
+            System.exit(0);
+        }
         result = n>0? "Positive" :n==0? "Zero" : "Negative";
         System.out.println(result);
+        
     }
-    
-}
