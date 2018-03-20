07f78aed10c2a3f288cb8037cf81d4170e4a781f
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 12:27:20 +0530

    Update Odd_Or_Even.java

--- Beginner/Set1/Odd_Or_Even.java
+++ Beginner/Set1/Odd_Or_Even.java
@@ -1,43 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package oddreven;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
-/**
- *
- * @author Sri Kumaran DR
- */
 public class OddReven {
-
-    /**
-     * @param args the command line arguments
-     */
    public static void main(String[] args) {
-        // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         String result="";
-        double n=0;
+        double j=0;
         try{
-            n = sc.nextDouble();
-            if(n>100000){
-            throw new InputMismatchException ("out of range");
+            j = sc.nextDouble();
+            if(j>100000){
+            throw new InputMismatchException ("out of the given range");
             }
         }
         catch(InputMismatchException e){
-                System.out.println("Input mismatch enter only integers and less than 100001");
+                System.out.println("Input mismatch enter oly integers and less than 1lakh and one 100001");
             System.exit(0);
-        }
-//        catch (IndexOutOfBoundsException o){
-//            //System.out.println("Out of range do not enter greater than 100000");
-//            throw new InputMismatchException("out of bounds");
-//        }
-        
-        result = n%2==0? "Even" : "Odd";
+        }        
+        result = j%2==0? "Even" : "Odd";
         System.out.println(result);
         
     }
