63317fc0cb08660ee719f292f4bb3055b5fa1ef3
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 16:11:58 +0530

    Create largestOfthreeNos.java

---
+++ Beginner/Set1/largestOfthreeNos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vowelRconsonant;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Kumaran DR
+ */
+public class vowelRconsonant {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array;
+        int i;
+        array = new int[10];
+        for(i=0;i<3;i++){
+            if(sc.hasNextInt())
+                array[i]=sc.nextInt();
+            else 
+                System.out.println("Invalid Input");
+        }
+        if((array[0]>array[2])&&(array[0]>array[1]))    
+            System.out.println(array[0]);
+        else if((array[1]>array[2])&&(array[1]>array[0]))
+            System.out.println(array[1]);
+        else
+            System.out.println(array[2]);
+        
+    }
+    
+}
