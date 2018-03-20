f80118a10714188345158c11f01869215baa483e
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 15:36:08 +0530

    Update AlphabetRnot.java

--- Beginner/Set1/AlphabetRnot.java
+++ Beginner/Set1/AlphabetRnot.java
@@ -21,7 +21,7 @@ public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         char c = sc.next().charAt(0);
-        if(((c>='a')&&(c<='z'))||((c>='A')&&(c<='Z'))){
+        if(((c>='A')&&(c<='Z'))||((c>='a')&&(c<='z'))){
             System.out.println("Alphabet");
         }
         else 
