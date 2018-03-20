a4533734dded8f55bf7ab15fb747152e476a6e6a
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 11:34:37 +0530

    Update Pos_or_Neg_or_zero.java

--- Beginner/Set1/Pos_or_Neg_or_zero.java
+++ Beginner/Set1/Pos_or_Neg_or_zero.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codekata.beginner.set1;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,25 +14,29 @@ import java.util.Scanner;
  */
 public class positiveOrNegOrZero {
 
+    /**
+     * @param args the command line arguments
+     */
+   
     public static void main(String[] args) {
-
+        // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         String result="";
         double n=0;
         try{
             n = sc.nextDouble();
             if(n>100000){
-            throw new IndexOutOfBoundsException("out of range");
+            throw new InputMismatchException("out of range");
             }
         }
         catch(InputMismatchException e){
-                System.out.println("Input mismatch enter only integers not grater than 100000");
+                System.out.println("Input mismatch enter only integers and less than 100001");
             System.exit(0);
         }
-        catch (IndexOutOfBoundsException o){
-            //System.out.println("Out of range do not enter greater than 100000");
-            throw new InputMismatchException("out of bounds");
-        }
+//        catch (IndexOutOfBoundsException o){
+//            //System.out.println("Out of range do not enter greater than 100000");
+//            throw new InputMismatchException("out of bounds");
+//        }
         
         result = n>0? "Positive" :n==0? "Zero" : "Negative";
         System.out.println(result);
