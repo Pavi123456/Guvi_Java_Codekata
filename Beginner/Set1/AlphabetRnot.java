d5bb912072a9de31dc31eaa59d38a1a1d4f5a15e
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 15:31:39 +0530

    Create AlphabetRnot.java

---
+++ Beginner/Set1/AlphabetRnot.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vowelRconsonant;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Kumaran DR
+ */
+public class vowelRconsonant {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        char c = sc.next().charAt(0);
+        if(((c>='a')&&(c<='z'))||((c>='A')&&(c<='Z'))){
+            System.out.println("Alphabet");
+        }
+        else 
+            System.out.println("Not an alphabet");
+    }
+    
+}
