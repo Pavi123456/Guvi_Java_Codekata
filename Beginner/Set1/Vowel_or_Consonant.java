7856c959fa4217e9d5b83db29f64064fb5e18bfb
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 15:28:17 +0530

    Update Vowel_or_Consonant.java

--- Beginner/Set1/Vowel_or_Consonant.java
+++ Beginner/Set1/Vowel_or_Consonant.java
@@ -21,10 +21,14 @@ public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         char c = sc.next().charAt(0);
+        if(((c>='a')&&(c<='z'))||((c>='A')&&(c<='Z'))){
         if((c=='a')||(c=='A')||(c=='e')||(c=='E')||(c=='i')||(c=='I')||(c=='o')||(c=='O')||(c=='u')||(c=='U'))
             System.out.println("Vowel");
         else  
             System.out.println("Consonant");
+        }
+        else 
+            System.out.println("Enter only letters");
     }
     
 }
