274c8c35823ffc3d91c45ede9d7e192ccc476cdd
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 11:13:54 +0530

    Update Pos_or_Neg_or_zero.java

--- Beginner/Set1/Pos_or_Neg_or_zero.java
+++ Beginner/Set1/Pos_or_Neg_or_zero.java
@@ -1,4 +1,3 @@
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,20 +11,20 @@ public class positiveOrNegOrZero {
 
         Scanner sc = new Scanner(System.in);
         String result="";
-        int n=0;
+        double n=0;
         try{
-            n = sc.nextInt();
+            n = sc.nextDouble();
             if(n>100000){
             throw new IndexOutOfBoundsException("out of range");
             }
         }
         catch(InputMismatchException e){
-                System.out.println("Input mismatch enter only integers");
+                System.out.println("Input mismatch enter only integers and less than 100001");
             System.exit(0);
         }
         catch (IndexOutOfBoundsException o){
-            System.out.println("Out of range do not enter greater than 100000");
-            System.exit(0);
+            //System.out.println("Out of range do not enter greater than 100000");
+            throw new InputMismatchException("out of bounds");
         }
         
         result = n>0? "Positive" :n==0? "Zero" : "Negative";
