44f592b0d065b7e915406239a3ceacd4c2f536a7
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-03-20 09:07:05 +0530

    Update Pos_or_Neg_or_zero.java

--- Beginner/Set1/Pos_or_Neg_or_zero.java
+++ Beginner/Set1/Pos_or_Neg_or_zero.java
@@ -1,1 +1,21 @@
 
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Kumaran DR
+ */
+public class positiveOrNegOrZero {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String result="";
+        int n = sc.nextInt();
+        result = n>0? "Positive" :n==0? "Zero" : "Negative";
+        System.out.println(result);
+    }
+    
+}
