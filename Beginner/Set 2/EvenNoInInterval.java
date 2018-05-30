5088cdb7a6c2f61f9e17736b1cffde3d4bcab901
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-05-30 17:51:07 +0530

    Create EvenNoInInterval.java

---
+++ Beginner/Set 2/EvenNoInInterval.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,i=0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if((a<=100000)&&(b<=100000)){
+            if(a%2==0){
+                i=a;
+                while(i<=b){
+                    System.out.print(i+" ");
+                    i+=2;
+                }
+            }
+            else{
+                i=a+1;
+                while(i<=b){
+                    System.out.print(i+" ");
+                    i+=2;
+                }
+            }
+        }
+        else{
+            System.out.println("Enter no. within 100000 only");
+        }
+    }
+}
