4a6050fb8aa7c23302f0294ec42c442fa426ea78
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-06-01 23:21:17 +0530

    Update Smallest_No_after_deleting_K_digits.java

--- Pro/Set1/Smallest_No_after_deleting_K_digits.java
+++ Pro/Set1/Smallest_No_after_deleting_K_digits.java
@@ -3,19 +3,19 @@ import java.lang.Math;
 public class AL{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int ip=sc.nextInt();
+        long ip = sc.nextLong();
         System.out.println("Ip from user = "+ip);
-        int d=sc.nextInt();
+        long d=sc.nextLong();
         System.out.println("D from user Nos to delete = "+d);
-        int k,div,result,n;
+        long k,div,result,n;
         n=result=div=0;
-        k=(int)(Math.log10(ip)+1);
+        k=(long)(Math.log10(ip)+1);
         System.out.println("No of terms in the integer"+k);
         for(int i=0;i<=d-1;i++){
             k=k-1;
         }
         System.out.println("Val of k before exp= "+k);
-        div=(int)Math.pow(10,k);
+        div=(long)Math.pow(10,k);
         System.out.println("divisior = "+div);
         result = ip/div;
         result=result*div;
