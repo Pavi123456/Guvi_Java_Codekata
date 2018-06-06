5f76aef8550191e824cda1d70c233a7c6d66e6d9
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-06-06 11:29:23 +0530

    Update ArrayIndexEqualsArrayVal.java

--- Hunter/Set1/ArrayIndexEqualsArrayVal.java
+++ Hunter/Set1/ArrayIndexEqualsArrayVal.java
@@ -4,11 +4,15 @@ public class RepeatedNos{
         Scanner sc = new Scanner(System.in);
         int a[]=new int[100000];
         int n=sc.nextInt();
+        String s= new String("");
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
             if(a[i] == i){
-                System.out.print(a[i]+" ");
+                s = s+a[i]+" ";
             }
         }
+        int pos = s.lastIndexOf(" ");
+        s = s.substring(0,pos);
+        System.out.println(s);
     }
 }
