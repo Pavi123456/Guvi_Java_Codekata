afdcddfb847de7e7c8222ec5f995dd14b013d667
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-06-06 15:33:29 +0530

    Update OddIndexEvnNosAnsEvnIndexOddNos.java

--- Hunter/Set1/OddIndexEvnNosAnsEvnIndexOddNos.java
+++ Hunter/Set1/OddIndexEvnNosAnsEvnIndexOddNos.java
@@ -4,14 +4,22 @@ public class RepeatedNos{
         Scanner sc = new Scanner(System.in);
         int a[]=new int[100000];
         int n =sc.nextInt();
+        String s = new String("");
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
             if((i%2==0)&&(a[i]%2==1)){
-                System.out.print(a[i]+" ");
+                //System.out.print(a[i]+" ");
+                s=s+a[i]+" ";
             }
             else if((i%2==1)&&(a[i]%2==0)){
-                System.out.print(a[i]+" ");
-            } 
+                //System.out.print(a[i]+" ");
+                s=s+a[i]+" ";
+            }
+            //System.out.println(s+"--");
+            if(i==n-1){
+                s=s.substring(0,s.length()-1);
+                System.out.println(s);
+            }
         }
     }
 }
