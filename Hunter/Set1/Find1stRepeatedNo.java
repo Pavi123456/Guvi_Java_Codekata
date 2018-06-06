0e11c4c328949a551e15dfac2f1a4390877eddf6
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-06-06 15:02:30 +0530

    Create Find1stRepeatedNo.java

---
+++ Hunter/Set1/Find1stRepeatedNo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class RepeatedNos{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a[]=new int[100000];
+        int n =sc.nextInt();
+        String s = new String("");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            s=s+a[i];
+        }
+        for(int i=0;i<n;i++){
+            int pos = s.lastIndexOf(s.charAt(i));
+            if(pos!=i){
+                System.out.println(s.charAt(i));
+                break;
+            }
+        }
+       
+    }
+}
