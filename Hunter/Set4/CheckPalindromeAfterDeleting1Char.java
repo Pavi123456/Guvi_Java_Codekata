93e8a0a90c304a60039d274d4849bd842efe2a8e
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-06-21 16:00:11 +0530

    Update CheckPalindromeAfterDeleting1Char.java

--- Hunter/Set4/CheckPalindromeAfterDeleting1Char.java
+++ Hunter/Set4/CheckPalindromeAfterDeleting1Char.java
@@ -4,13 +4,20 @@ public class string{
         Scanner sc = new Scanner(System.in);
         String s = new String("");
         s=sc.next();
+        s=s.toUpperCase();
         //System.out.println("Original String: "+s);
         int i=0;
         String s1,rev;
         rev = new String("");
         s1 = new String("");
         for(i=0;i<s.length();i++){
-            s1 = s.replace(String.valueOf(s.charAt(i)),"");
+            //s1 = s.replace(String.valueOf(s.charAt(i)),"");
+            if(i==0){
+                s1 = s.substring(1,s.length());
+            }
+            else{
+                s1 = s.substring(0,i)+s.substring(i+1,s.length());
+            }
             for(int j=s1.length()-1;j>=0;j--){
                 rev = rev + s1.charAt(j);   
                 //System.out.println("Rev: "+rev);
