9def70ec1ecac6a46a8d8faba7fd35a6379ccfc0
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-06-27 17:03:47 +0530

    Create TestHunter7.java

---
+++ Hunter/Set1/TestHunter7.java
@@ -0,0 +1,28 @@
+//testing changes in line//
+import java.util.Scanner;
+public class RepeatedNos{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a[]=new int[100000];
+        int n =sc.nextInt();
+        String s = new String("");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            if((i%2==0)&&(a[i]%2==1)){
+                //System.out.print(a[i]+" ");
+                s=s+a[i-2]+" ";
+            }
+            else if((i%2==1)&&(a[i]%2==0)){
+                //System.out.print(a[i]+" ");
+                s=s+a[i-2]+" ";
+            }
+            //System.out.println(s+"--");
+            if(i==n-1){
+                s=s.substring(0,s.length()-1);
+                System.out.println(s);
+            }
+        }
+    }
+}
+
+//testing changes in line//
