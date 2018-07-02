37dabf4cac43436260893ce84dba7286af91404b
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-07-02 15:53:18 +0530

    Update RevNumber3.java

--- Player/Set1/RevNumber3.java
+++ Player/Set1/RevNumber3.java
@@ -5,7 +5,7 @@ public class StringRev{
         Scanner sc = new Scanner(System.in);
         String s = new String("");
         s= sc.next();
-        for(int i = s.length()-1;i>0;i--)
+        for(int i = s.length()-1;i>=0;i--)
             System.out.print(s.charAt(i));
     }
 }
