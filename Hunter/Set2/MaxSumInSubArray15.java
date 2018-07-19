e3b3d03a110f6eeb34c1edbb8abad321d8bd7453
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-07-19 17:42:30 +0530

    Update MaxSumInSubArray15.java

--- Hunter/Set2/MaxSumInSubArray15.java
+++ Hunter/Set2/MaxSumInSubArray15.java
@@ -5,10 +5,23 @@ public class MyClass {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum=0;
-        for(int i =0; i <n;i++){
-            //a[i] = sc.nextInt();
-            sum = sum + sc.nextInt(); 
+        
+        for(int i =0; i<n;i++){
+            a[i] = sc.nextInt();
+            //sum = sum + a[i];
         }
-        System.out.println(sum);
+        for(int i=0;i<n;i++){
+            int sumt=0;    
+            for(int j=i;j<n;j++){
+                if(a[i]>=sum){
+                    sum = a[i];
+                }
+                sumt = sumt+a[j];
+                if(sumt>sum){
+                    sum=sumt;
+                }
+            }//System.out.println(sum);
+        }
+       System.out.println(sum);
     }
 }
