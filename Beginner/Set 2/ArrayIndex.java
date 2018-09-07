f467ce0137c89a0dc07dc4d0b91abbbdc8dc2daa
SriKum9921 <32298064+SriKum9921@users.noreply.github.com>
2018-09-07 22:45:19 +0530

    Update ArrayIndex.java

--- Beginner/Set 2/ArrayIndex.java
+++ Beginner/Set 2/ArrayIndex.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class ArrayIndex{
     public static void main(String[] args){
-        int a[] = new int[1000];
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
